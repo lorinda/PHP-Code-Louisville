2eabdb2f7fe27f92ee3d94df55133cc128150511
Lorinda <lorinda@users.noreply.github.com>
2017-01-16 15:33:12 -0500

    Usage of echo implode, array_push, array_unshift, array_shift, array_pop, unset, array_values

---
+++ PHP-Arrays-and-Control-Structures/indexed_arrays.php
@@ -0,0 +1,23 @@
+<?php 
+$learn = array('Conditionals', 'Arrays', 'Loops');
+$learn[] = 'Add'; //adds at end
+array_push($learn,'Functions','Forms','Objects'); //adds to end
+array_unshift($learn,'HTML','CSS');  //adds to beginning
+//var_dump($learn); //shows index, type, value
+//echo $learn[1];  //displays 2nd element
+
+echo 'You removed '.array_shift($learn)."\n"; //to remove from beginning
+echo "You removed ".array_pop($learn)."\n";  //to remove from end
+
+echo implode("\n",$learn);  //Displays separator, value at each index
+echo PHP_EOL;
+unset($learn[1],$learn[2]); //Deletes but does not override indices
+var_dump($learn);
+
+$learn = array_values($learn); //renumbers indices
+var_dump($learn);
+
+$learn[0] = "Email"; //Redefines value at key/index
+
+
+?>
