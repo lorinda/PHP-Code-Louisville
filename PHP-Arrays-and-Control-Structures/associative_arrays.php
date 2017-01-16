55201bfb081d7d52c978680ff7d15b07125ba572
Lorinda <lorinda@users.noreply.github.com>
2017-01-16 15:51:13 -0500

    Add examples of string and integer keys

---
+++ PHP-Arrays-and-Control-Structures/associative_arrays.php
@@ -0,0 +1,18 @@
+<?php
+$iceCream = array("Alena" => "Black Cherry",
+                 "Treasure" => "Chocolate",
+                 "Dave" => "Cookies and Cream",
+                 "Rialla" => "Strawberry"
+                 );  //Define case-sensitive keys
+$iceCream[] = 'Vanilla'; //assigns integer beginning with 0
+var_dump($iceCream);
+echo PHP_EOL.$iceCream['Alena'].PHP_EOL;
+
+$keys = array(
+        1 => 'a',
+        '1' => 'b',
+        1.5 => 'c',
+        true => 'd'    
+        ); //result is key 1 with value d
+var_dump($keys);
+?>
