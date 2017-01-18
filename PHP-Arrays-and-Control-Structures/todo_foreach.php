6fc5c08cdaa0848e074724025473643a0ec45c91
Lorinda <lorinda@users.noreply.github.com>
2017-01-17 22:21:23 -0500

    Remove array comments

--- PHP-Arrays-and-Control-Structures/todo_foreach.php
+++ PHP-Arrays-and-Control-Structures/todo_foreach.php
@@ -31,27 +31,4 @@ echo '</table>';
 
 //var_dump($list);
 
-
-//Previous Example:
-//$task1 = array(
-//replace array() with []"
-//$list['Task 1'] = [
-//    'title' => 'Laundry',
-//    'priority' => 2,
-//    'due' => '',
-//    'complete' => true
-//    ];
-////$task2 = array(
-//$list['Task 2'] = [
-//    'title' => "Clean out refrigerator",
-//    'priority' => 3,
-//    'due' => '07/30/2016',
-//    'complete' => false
-//    ];
-
-//$list = array($task1, $task2);
-//var_dump($task1);
-//var_dump($task2);
-
-//echo $list['Task 1']['title'];
 ?>
