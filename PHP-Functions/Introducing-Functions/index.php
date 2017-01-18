1cf7efe09b01dcdd4c90b960512d7559444f7ec7
Lorinda <lorinda@users.noreply.github.com>
2017-01-18 14:17:05 -0500

    Add pass by value, reference/make variable global

--- PHP-Functions/Introducing-Functions/index.php
+++ PHP-Functions/Introducing-Functions/index.php
@@ -1,91 +1,86 @@
 <?php
 
 //Create a function name(){}
-function Hello(){
-    echo "Hello, World";
-}
-$current_user = "Mike";
-function is_mike(){
-    global $current_user;
-    if($current_user == "Mike"){
-        echo "It is Mike";
+//function Hello($name){
+//    echo "Hello, $name";
+//}
+//
+//Hello("Lorinda");
+
+//passing argument by value: the default
+
+//passing argument by reference: value can change through function
+
+//function hello($name){
+//    echo "Hello, $name, how's it going!";
+//}
+
+//Call the function
+//hello("Hampton");
+
+
+function hello($arr = 'friends'){
+    if(is_array($arr)){
+        //loop through $arr
+        foreach($arr as $name){
+            
+            //<br> in echo for new line
+            echo "Hello, $name. How's it going!<br>";
+        }
     }else{
-        echo "It is not Mike";
+        echo "Hello, $arr, how's it going?";
+    }
+}
+
+$names = array(
+    'Bob',
+    'Shelly');
+
+
+hello($names);
+
+
+
+$current_user = "Mike";
+
+
+function is_mike(){
+    //global variable outside of function used in function
+    global $current_user;
+    if($current_user == 'Mike'){
+        echo '<html>It is Mike </html>';
+    }
+    else{
+        echo 'It is not Mike';
     }
 }
 
 is_mike();
-//passing by value: the default
 
-//passing by reference: value can change through function
 
-/*function hello($name){
-    echo "Hello, $name, how's it going!";
+
+//PHP Function Default Arguments
+//optional argument = NULL
+    //Check with if statement
+    //example: $title = NULL;
+//default argument = "phrase"
+    //example: $title = 'friend';
+function get_info($name, $title=NULL){
+   
+    if($title){
+        
+        echo "<br>$name has arrived.  $name is with us as a $title";    
+        
+    }
+    else{
+        echo "<br>$name has arrived. Welcome!";
+        
+    }
+
+
 }
 
-//Call the function
-hello("Hampton");*/
-
-
-//function hello($arr = 'friends'){
-//    if(is_array($arr)){
-//        foreach($arr as $name){
-//            
-//            //<br> in echo for new line
-//            echo "Hello, $name. How's it going!<br>";
-//        }
-//    }else{
-//        echo "Hello, $arr, how's it going?";
-//    }
-//}
-//
-//$names = array(
-//    'Bob',
-//    'Shelly');
-//
-//
-//hello($names);
-//
-//
-//
-//$current_user = "Mike";
-//
-//
-//function is_mike(){
-//    //global variable outside of function used in function
-//    global $current_user;
-//    if($current_user == 'Mike'){
-//        echo '<html>It is Mike </html>';
-//    }
-//    else{
-//        echo 'It is not Mike';
-//    }
-//}
-//
-//is_mike();
-//
-//
-//
-////PHP Function Default Arguments
-////optional argument = NULL
-//    //Check with if statement
-////default argument = "phrase"
-//function get_info($name, $title=NULL){
-//   
-//    if($title){
-//        
-//        echo "<br>$name has arrived.  $name is with us as a $title";    
-//        
-//    }
-//    else{
-//        echo "<br>$name has arrived. Welcome!";
-//        
-//    }
-//
-//
-//}
-//
-//get_info('Mike','frog');
+get_info('Mike','frog');
 
 
 ?>
