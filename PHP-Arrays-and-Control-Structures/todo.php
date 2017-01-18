b17893dd3de0d63140e37390a5c489d38796e664
Lorinda <lorinda@users.noreply.github.com>
2017-01-17 20:38:47 -0500

    Add foreach loops for to do items

--- PHP-Arrays-and-Control-Structures/todo.php
+++ PHP-Arrays-and-Control-Structures/todo.php
@@ -1,13 +1,40 @@
 <?php
+//header('Content-type: text/plain');
 include 'list.php';
 
+foreach($list as $key => $item){
+    echo $key.' = '.$item['title']."<br>\n";
+}
+echo '<table>';
+echo "<tr>";
+echo "<th>Title</th>";
+echo "<th>Priority</th>";
+echo "<th>Due Date</th>";
+echo "<th>Complete</th>";
+echo "</tr>";
 
-var_dump($list);
+foreach($list as $item){
+    echo '<tr>';
+    echo '<td>'.$item['title']."</td>\n";
+    echo '<td>'.$item['priority']."</td>\n";
+    echo '<td>'.$item['due']."</td>\n";
+    echo '<td>';
+    if($item['complete']){
+        echo 'Yes';
+    }else{
+        echo 'No';
+    }
+    echo "</td>\n";
+    echo '</tr>';
+}
+echo '</table>';
+
+//var_dump($list);
 
 
 //Previous Example:
 //$task1 = array(
-//replace array() with []
+//replace array() with []"
 //$list['Task 1'] = [
 //    'title' => 'Laundry',
 //    'priority' => 2,
