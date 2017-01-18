6bc09171383c3d9ab06c3ef3adbfadf92377a143
Lorinda <lorinda@users.noreply.github.com>
2017-01-17 22:48:32 -0500

    Order to do list by priority key

---
+++ PHP-Arrays-and-Control-Structures/todo_ordered_foreach.php
@@ -0,0 +1,60 @@
+<?php
+//header('Content-type: text/plain');
+include 'list.php';
+
+$status = 'all';
+$field = 'priority';
+$order = array();
+
+if ($status == 'all'){
+    $order = array_keys($list);
+}else{
+    foreach($list as $key => $item){
+        if($item['complete'] == $status){
+            $order[] = $key;
+        }
+
+    }    
+}
+if ($field){
+    $sort = array();
+    foreach($order as $id){
+        $sort[$id] = $list[$id][$field];
+    }
+    asort($sort);
+    $order = array_keys($sort);
+}
+
+//var_dump($sort);
+//var_dump($list);
+
+//var_dump($order);
+//var_dump($list);
+
+echo '<table>';
+echo "<tr>";
+echo "<th>Title</th>";
+echo "<th>Priority</th>";
+echo "<th>Due Date</th>";
+echo "<th>Complete</th>";
+echo "</tr>";
+
+foreach($order as $id){
+    echo '<tr>';
+    echo '<td>'.$list[$id]['title']."</td>\n";
+    echo '<td>'.$list[$id]['priority']."</td>\n";
+    echo '<td>'.$list[$id]['due']."</td>\n";
+    echo '<td>';
+    if($list[$id]['complete']){
+        echo 'Yes';
+    }else{
+        echo 'No';
+    }
+    echo "</td>\n";
+    echo '</tr>';
+}
+echo '</table>';
+
+//var_dump($list);
+
+?>
