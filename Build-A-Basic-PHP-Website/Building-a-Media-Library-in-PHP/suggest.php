65034644b85c0d3234d4e71486be014e358ee180
Lorinda <lorinda@users.noreply.github.com>
2017-01-23 15:03:04 -0500

    Add  variables to check for page active status

--- Build-A-Basic-PHP-Website/Building-a-Media-Library-in-PHP/suggest.php
+++ Build-A-Basic-PHP-Website/Building-a-Media-Library-in-PHP/suggest.php
@@ -1,6 +1,9 @@
 <?php 
 $pageTitle = "Suggest a Media Item";
-include("inc/header.php");?>
+$section = "suggest";
+
+include("inc/header.php");
+?>
 
 <div class="section page">
     <h1>Suggest a Media Item</h1>
