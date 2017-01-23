50424f8312d7b6948595172ac26de03f95c92988
Lorinda <lorinda@users.noreply.github.com>
2017-01-23 18:01:22 -0500

    Create array_category() function to output array keys with matching category values

--- Build-A-Basic-PHP-Website/Listing-and-Sorting-Inventory-Items/inc/functions.php
+++ Build-A-Basic-PHP-Website/Listing-and-Sorting-Inventory-Items/inc/functions.php
@@ -7,4 +7,13 @@ function get_item_html($id, $item){
             ."</a></li>";  
     return $output;
 }     
-     
+function array_category($catelog, $category){
+    $output = array();
+    
+    foreach($catalog as $id => $item){
+        if (strtolower($category) == strtolower($item["category"]){
+            $output[] = $id;
+        }
+    }
+            return $output;
+}
