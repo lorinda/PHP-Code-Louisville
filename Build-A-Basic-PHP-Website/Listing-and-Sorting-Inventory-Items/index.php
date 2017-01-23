80aa1d445725ad629f35960e9f63fd4983ebc583
Lorinda <lorinda@users.noreply.github.com>
2017-01-23 17:42:31 -0500

    Use array_rand() function to display random catalog items (lines 18-20)

--- Build-A-Basic-PHP-Website/Listing-and-Sorting-Inventory-Items/index.php
+++ Build-A-Basic-PHP-Website/Listing-and-Sorting-Inventory-Items/index.php
@@ -15,8 +15,9 @@ include("inc/functions.php");
 
 				<ul class="items">
 				    <?php
-                        foreach($catalog as $id => $item){
-                        echo get_item_html($id, $item);
+                        $random = array_rand($catalog, 4);
+                        foreach($random as $id => $item){
+                            echo get_item_html($id, $catalog[$item]);
                     }
                     ?>	
                 </ul>
