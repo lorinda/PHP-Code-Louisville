c019a0a1020c4153a29a54aaa257ee7e59205276
Lorinda <lorinda@users.noreply.github.com>
2017-01-23 22:24:39 -0500

    Use  array to retrieve form values submitted

---
+++ Build-A-Basic-PHP-Website/Adding-a-Basic-Form/process.php
@@ -0,0 +1,11 @@
+<!--Processing form data-->
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$details = $_POST["details"];
+
+echo $name."<br>";
+echo $email."<br>";
+echo $details."<br>";
+?>
+    
