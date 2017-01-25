d014347a6be6464962b6625d0396c8480bf743c4
Lorinda <lorinda@users.noreply.github.com>
2017-01-25 13:53:50 -0500

    Include header and footer and 'section page' div

--- Build-A-Basic-PHP-Website/Adding-a-Basic-Form/process.php
+++ Build-A-Basic-PHP-Website/Adding-a-Basic-Form/process.php
@@ -4,12 +4,22 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $details = $_POST["details"];
 
-Echo "<pre>";
+echo "<pre>";
 $email_body = '';
 $email_body .= "Name: ".$name."\n";
 $email_body .= "Email: ".$email."\n";
 $email_body .= "Details: ".$details."\n";
 echo $email_body;
 echo "</pre>";
+
+$pageTitle = "Thank you";
+$section = null;
+include("inc/header.php");
 ?>
-    
+
+<div class="section page">
+    <h1>Thank You</h1>
+    <p>Thanks for the email! I &rsquo;ll check out your suggestion shortly!</p>
+</div><!--End div "section page"-->
+
+<?php include("inc/footer.php");
