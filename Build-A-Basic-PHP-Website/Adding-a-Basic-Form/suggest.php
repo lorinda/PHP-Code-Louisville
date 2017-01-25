3b3e91c9608a210fef047e9c2126541066bc070a
Lorinda <lorinda@users.noreply.github.com>
2017-01-25 15:23:41 -0500

    Add phpmailer properties and methods to send email

--- Build-A-Basic-PHP-Website/Adding-a-Basic-Form/suggest.php
+++ Build-A-Basic-PHP-Website/Adding-a-Basic-Form/suggest.php
@@ -25,15 +25,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         exit;
     }
     
-    echo "<pre>";
     $email_body = '';
     $email_body .= "Name: ".$name."\n";
     $email_body .= "Email: ".$email."\n";
     $email_body .= "Details: ".$details."\n";
-    echo $email_body;
-    echo "</pre>";
-
+    
+    
     //Send Email
+    $mail->setFrom($email, $name);
+    $mail->addAddress('treehouse@localhost', 'Lorinda');     // Add a recipient
+    
+    $mail->isHTML(false);                                  // Set email format to HTML
+
+    $mail->Subject = 'Personal Media Library Suggestion from '.$name;
+    $mail->Body    = $email_body;
+    
+    //calls send method
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+        exit;
+    } 
+    
+    //Redirect
     header("location:suggest.php?status=thanks");
 
 }
