f7838a51ec1e1afc68c541673237b64453abc812
Lorinda <lorinda@users.noreply.github.com>
2017-01-25 14:19:29 -0500

    Add thanks.php divs, thanks.php no longer needed

--- Build-A-Basic-PHP-Website/Adding-a-Basic-Form/suggest.php
+++ Build-A-Basic-PHP-Website/Adding-a-Basic-Form/suggest.php
@@ -1,4 +1,23 @@
 <?php 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $details = $_POST["details"];
+
+    echo "<pre>";
+    $email_body = '';
+    $email_body .= "Name: ".$name."\n";
+    $email_body .= "Email: ".$email."\n";
+    $email_body .= "Details: ".$details."\n";
+    echo $email_body;
+    echo "</pre>";
+
+    //Send Email
+    header("location:suggest.php?status=thanks");
+
+}
+
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
@@ -8,8 +27,12 @@ include("inc/header.php");
 <div class="section page">
     <div class="wrapper">
         <h1>Suggest a Media Item</h1>
+        <?php 
+        if (isset($_GET["status"]) && $_GET["status"] == "thanks"){
+            echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+        }else{ ?>
         <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email</p>
-        <form method="post" action="process.php">
+        <form method="post" action="suggest.php">
             <table>
                 <tr>
                     <th><label for="name_id">Name</label></th>
@@ -27,6 +50,7 @@ include("inc/header.php");
             <input type="submit" value="Send" />
             
         </form>
+        <?php }?><!--End else block-->
     </div>
 </div>
 <?php include("inc/footer.php"); ?>
