e5e6e45f93744650d37f2e521d78a72163ad57b6
Lorinda <lorinda@users.noreply.github.com>
2017-01-25 12:52:13 -0500

    Add <pre> tag to format screen output of \$email_body

--- Build-A-Basic-PHP-Website/Adding-a-Basic-Form/process.php
+++ Build-A-Basic-PHP-Website/Adding-a-Basic-Form/process.php
@@ -4,8 +4,12 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $details = $_POST["details"];
 
-echo $name."<br>";
-echo $email."<br>";
-echo $details."<br>";
+Echo "<pre>";
+$email_body = '';
+$email_body .= "Name: ".$name."\n";
+$email_body .= "Email: ".$email."\n";
+$email_body .= "Details: ".$details."\n";
+echo $email_body;
+echo "</pre>";
 ?>
     
