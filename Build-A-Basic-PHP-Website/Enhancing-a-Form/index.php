f644f2cf6ce61c387481b929eb6a3059813121f7
Lorinda <lorinda@users.noreply.github.com>
2017-01-26 19:22:39 -0500

    Line 19-20: Fix 'View Details' links by changing => and  to

--- Build-A-Basic-PHP-Website/Enhancing-a-Form/index.php
+++ Build-A-Basic-PHP-Website/Enhancing-a-Form/index.php
@@ -16,8 +16,8 @@ include("inc/functions.php");
 				<ul class="items">
 				    <?php
                         $random = array_rand($catalog, 4);
-                        foreach($random as $id => $item){
-                            echo get_item_html($id, $catalog[$item]);
+                        foreach($random as $id){
+                            echo get_item_html($id, $catalog[$id]);
                     }
                     ?>	
                 </ul>
