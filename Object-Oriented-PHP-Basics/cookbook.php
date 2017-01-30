63f46aed0253b621f90f04cedcd1c3c60b986564
Lorinda <lorinda@users.noreply.github.com>
2017-01-30 16:59:08 -0500

    Start tutorial

---
+++ Object-Oriented-PHP-Basics/cookbook.php
@@ -0,0 +1,17 @@
+<?php
+include "classes/recipes.php";
+
+$recipe1 = new Recipe();
+$recipe1->source = "Grandma Holligan";
+$recipe1->setTitle("my first recipe");
+$recipe1->addIngredient("egg", 1, "doz");
+$recipe1->addIngredient("flour", 2, "cup");
+
+$recipe2 = new Recipe();
+$recipe2->source = "Betty Crocker";
+$recipe2->setTitle("my second recipe");
+
+echo $recipe1->getTitle();
+foreach ($recipe1->getIngredients() as $ing) {
+    echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
+}
