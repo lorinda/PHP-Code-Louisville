ac9c5181a5cdfc6b2dde08e563cfdb458b97a62b
Lorinda <lorinda@users.noreply.github.com>
2017-01-30 15:43:23 -0500

    Explore getters, setters and class modifiers

--- Object-Oriented-PHP-Basics/classes/recipe.php
+++ Object-Oriented-PHP-Basics/classes/recipe.php
@@ -1,14 +1,38 @@
 <?php
 class Recipe
 {
-    public $title;
+    private $title;
     public $dryIngredients = array();
     public $instructions = array();
     public $yield;
     public $tag = array();
     public $source = "Alena Holligan";
+    
+    public function setTitle($title){
+        $this->title = ucwords($title);//uppercase first word
+    }
+    
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    public function displayRecipe()
+    {
+        return $this->title." by ". $this->source;   
+    }
 }
 
 $recipe1 = new Recipe();
-var_dump($recipe1);
+$recipe1->source = "Grandma Holligan";
+$recipe1->setTitle("my first Recipe");
+
+$recipe2 = new Recipe();
+$recipe2->source = "Duncan Hines";
+$recipe2->setTitle("My Second Recipe");
+
+echo $recipe1->displayRecipe().PHP_EOL;
+echo $recipe2->displayRecipe().PHP_EOL;
+echo $recipe1->getTitle();
+
 ?>
