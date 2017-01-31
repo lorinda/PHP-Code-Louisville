69ae94a1300e4d5dad26e42baf0600c7785d1784
Lorinda <lorinda@users.noreply.github.com>
2017-01-31 16:08:27 -0500

    Line 6: Display recipe. echo Render::displayRecipe();

--- Object-Oriented-PHP-Basics/cookbook.php
+++ Object-Oriented-PHP-Basics/cookbook.php
@@ -1,29 +1,36 @@
 <?php
 include "classes/recipes.php";
 include "classes/render.php";
+include "inc/recipes.php";
 
-$recipe1 = new Recipe("my first recipe");
-$recipe1->setSource = "Grandma Holligan";
-$recipe1->addIngredient("egg", 1, "doz");
-$recipe1->addIngredient("flour", 2, "cup");
-
-$recipe2 = new Recipe("my second recipe");
-$recipe2->setSource = "Duncan Hines";
-$recipe1->addInstruction("This is my first instruction");
-$recipe1->addInstruction("This is my second instruction");
-
-$recipe1->addTag("Breakfast");
-$recipe1->addTag("Main Course");
-
-$recipe1->setYield("6 servings");
+echo Render::displayRecipe($belgian_waffles);
 
 
-//Call static method
-//echo Render::displayRecipe($recipe1);
 
-//add to_string method in cookbook.php to display recipe title
-echo $recipe1."\n";
-echo new Render();
+
+//Comment: Previous code. Removed for include "inc/recipes.php"
+//$recipe1 = new Recipe("my first recipe");
+//$recipe1->setSource = "Grandma Holligan";
+//$recipe1->addIngredient("egg", 1, "doz");
+//$recipe1->addIngredient("flour", 2, "cup");
+//
+//$recipe2 = new Recipe("my second recipe");
+//$recipe2->setSource = "Duncan Hines";
+//$recipe1->addInstruction("This is my first instruction");
+//$recipe1->addInstruction("This is my second instruction");
+//
+//$recipe1->addTag("Breakfast");
+//$recipe1->addTag("Main Course");
+//
+//$recipe1->setYield("6 servings");
+//
+//
+////Call static method
+////echo Render::displayRecipe($recipe1);
+//
+////add to_string method in cookbook.php to display recipe title
+//echo $recipe1."\n";
+//echo new Render();
 
 //Commented: Previous Echo statements
 //echo $recipe1->getTitle();
