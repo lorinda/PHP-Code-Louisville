40ede43fad810631071aa0eb0afd0f870005d1d2
Lorinda <lorinda@users.noreply.github.com>
2017-01-31 17:31:07 -0500

    Add functionality to list recipe titles

--- Object-Oriented-PHP-Basics/cookbook.php
+++ Object-Oriented-PHP-Basics/cookbook.php
@@ -23,7 +23,7 @@ $cookbook->addRecipe($corn_beef_hash);
 $cookbook->addRecipe($granola);
 $cookbook->addRecipe($spicy_omelette);
 $cookbook->addRecipe($scones);
-var_dump($cookbook);
+echo Render::listRecipes($cookbook->getRecipeTitles());
 //echo Render::displayRecipe($belgian_waffles);
 
 
