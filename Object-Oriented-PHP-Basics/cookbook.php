93acf6acf3447630c47cd2fa865f850ec5d44cdc
Lorinda <lorinda@users.noreply.github.com>
2017-01-30 22:57:50 -0500

    Display $recipe1->getInstructions()

--- Object-Oriented-PHP-Basics/cookbook.php
+++ Object-Oriented-PHP-Basics/cookbook.php
@@ -2,16 +2,21 @@
 include "classes/recipes.php";
 
 $recipe1 = new Recipe();
-$recipe1->source = "Grandma Holligan";
+//$recipe1->source = "Grandma Holligan";
 $recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg", 1, "doz");
 $recipe1->addIngredient("flour", 2, "cup");
 
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+//$recipe2->source = "Betty Crocker";
 $recipe2->setTitle("my second recipe");
 
 echo $recipe1->getTitle();
 foreach ($recipe1->getIngredients() as $ing) {
     echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
 }
+
+$recipe1->addInstruction("This is my first instruction");
+$recipe1->addInstruction("This is my second instruction");
+
+echo implode("\n", $recipe1->getInstructions());
