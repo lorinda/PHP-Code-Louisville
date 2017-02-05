da158af8f342ac11bee2b825eb655b41f3d62415
Lorinda <lorinda@users.noreply.github.com>
2017-02-05 16:47:55 -0500

    Set up function for handling uncaught (default) exceptions

--- Basic-Exception-Handling-with-PHP/exceptions.php
+++ Basic-Exception-Handling-with-PHP/exceptions.php
@@ -8,8 +8,17 @@ function exception_error_handler($severity, $message, $file, $line){
     throw new ErrorException($message, 0, $severity, $file, $line);
 }
 
-//define function for handling errors
+//set function for handling errors
 set_error_handler("exception_error_handler");
+
+
+//set up function for handling default (uncaught) exceptions
+function exception_handler($e){
+    echo "Uncaught exception ". $e->getMessage();
+}
+
+//set default exception handler
+set_exception_handler("exception_handler");
 
 try {
     strpos();
@@ -17,6 +26,8 @@ try {
     echo "ERROR! ".$e->getMessage().PHP_EOL;
 }
 
+//Will throw an exception 
+echo 10/0;
 
 echo "End of file";
 
