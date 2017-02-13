030322f2a3828c9e479417a35a32f36ee8e2eb3e
Lorinda <lorinda@users.noreply.github.com>
2017-02-13 18:21:07 -0500

    Copy paste full_catalog_array() function and rename as single_item_array(). Update the db query and add $id parameter to new function single_item_array()

--- Integrating-PHP-with-Databases/inc/functions.php
+++ Integrating-PHP-with-Databases/inc/functions.php
@@ -20,6 +20,32 @@ function full_catalog_array(){
     return $catalog;
 }
 
+function single_item_array($id){
+    include "connection.php";
+    
+    try{
+    //variable stores results of SELECT query
+    $results = $db->query("SELECT title, category, img, format, year, genre, publisher, isbn 
+                            FROM Media
+                            JOIN Genres ON Media.genre_id = Genres.genre_id
+                            LEFT OUTER JOIN Books ON Media.media_id = Books.media_id
+                            WHERE Media.media_id = $id"
+                         );
+    }catch(Exception $e){
+        echo "Unable to retrieve results";
+        exit;
+    
+    }
+    //echo "Retrieved Results";
+
+    //FETCH_ASSOC condenses reults to associative keys
+    //var_dump($results->fetchAll(PDO::FETCH_ASSOC));
+
+    //Generate catalog array from database PDO
+    $catalog = $results->fetchAll();
+    return $catalog;
+}
+
 function get_item_html($id,$item) {
     $output = "<li><a href='details.php?id="
         . $id . "'><img src='" 
