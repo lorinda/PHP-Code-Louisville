d2d9635ab6d426592d741270746eb9559ac9f17c
Lorinda <lorinda@users.noreply.github.com>
2017-02-13 22:51:25 -0500

    Populate item details with each person included as director, writer, etc

--- Integrating-PHP-with-Databases/details.php
+++ Integrating-PHP-with-Databases/details.php
@@ -6,7 +6,7 @@ include("inc/functions.php");
 if (isset($_GET["id"])) {
     $id = filter_input(INPUT_GET,"id",FILTER_SANITIZE_NUMBER_INT);
     $item = single_item_array($id);
-    var_dump($item);
+    //var_dump($item);
     
 }
 
@@ -63,7 +63,7 @@ include("inc/header.php"); ?>
                 <?php if (strtolower($item["category"]) == "books") { ?>
                 <tr>
                     <th>Authors</th>
-                    <td><?php echo implode(", ",$item["authors"]); ?></td>
+                    <td><?php echo implode(", ",$item["author"]); ?></td>
                 </tr>
                 <tr>
                     <th>Publisher</th>
@@ -76,20 +76,20 @@ include("inc/header.php"); ?>
                 <?php } else if (strtolower($item["category"]) == "movies") { ?>
                 <tr>
                     <th>Director</th>
-                    <td><?php echo $item["director"]; ?></td>
+                    <td><?php echo implode(", ",$item["director"]); ?></td>
                 </tr>
                 <tr>
                     <th>Writers</th>
-                    <td><?php echo implode(", ",$item["writers"]); ?></td>
+                    <td><?php echo implode(", ",$item["writer"]); ?></td>
                 </tr>
                 <tr>
                     <th>Stars</th>
-                    <td><?php echo implode(", ",$item["stars"]); ?></td>
+                    <td><?php echo implode(", ",$item["star"]); ?></td>
                 </tr>
                 <?php } else if (strtolower($item["category"]) == "music") { ?>
                 <tr>
                     <th>Artist</th>
-                    <td><?php echo $item["artist"]; ?></td>
+                    <td><?php echo implode(", ",$item["artist"]); ?></td>
                 </tr>
                 <?php } ?>
             </table>
