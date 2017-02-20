b43b2f8dd30645ab5251f00941d6b0616ab0d771
Lorinda <lorinda@users.noreply.github.com>
2017-02-20 16:38:46 -0500

    Create PDO connection

---
+++ CRUD-Operations-with-PHP/inc/connection.php
@@ -0,0 +1,12 @@
+<?php
+$user = "treehouse";
+$pd = "w9fj3l45";
+try{
+    $db = new PDO('mysql:host=localhost;dbname=crud_database', $user,$pd);
+    
+    //throw an exception for any error
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+    die($e->getMessage().PHP_EOL);
+}
+var_dump($db);
