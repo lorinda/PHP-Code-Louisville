6dd89e7a06c0db3ea35d1bc66dac18c0528d736f
Lorinda <lorinda@users.noreply.github.com>
2017-02-21 18:20:31 -0500

    Add add_task() function

--- CRUD-Operations-with-PHP/inc/functions.php
+++ CRUD-Operations-with-PHP/inc/functions.php
@@ -51,16 +51,24 @@ function get_task_list($filter = null){
     return $results->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function add_project($title, $category){
+function add_project($title, $category, $project_id = null){
     include 'connection.php';
     
-    $sql = 'INSERT INTO projects(title, category) 
+    if ($project_id){
+        $sql = 'UPDATE projects SET title = :title, category = :category 
+                WHERE project_id = :id';
+    }else{
+        $sql = 'INSERT INTO projects(title, category) 
             VALUES (:title, :category)';
+    }
     
     try{
         $results = $db->prepare($sql);
         $results->bindParam(":title", $title, PDO::PARAM_STR);
         $results->bindParam(":category", $category, PDO::PARAM_STR);
+        if($project_id){
+            $results->bindParam(":id", $project_id, PDO::PARAM_INT);
+        }
         $results->execute();
     }catch(Exception $e){
         echo "Error!: " . $e->getMessage() . "<br />";
@@ -85,11 +93,18 @@ function get_project($project_id){
     }
     return $results->fetch();
 }
-function add_task($project_id, $title, $date, $time){
+
+function add_task($project_id, $title, $date, $time, $task_id = null){
     include 'connection.php';
     
-    $sql = 'INSERT INTO tasks(project_id, title, date, time) 
+    if($task_id){
+        $sql = 'UPDATE tasks SET project_id = :project_id, title = :title, date = :date, time = :time
+                WHERE task_id = :task_id';
+    }else{
+        $sql = 'INSERT INTO tasks(project_id, title, date, time) 
             VALUES (:project_id, :title, :date, :time)';
+        
+    }
     
     try{
         $results = $db->prepare($sql);
@@ -97,10 +112,30 @@ function add_task($project_id, $title, $date, $time){
         $results->bindParam(":title", $title, PDO::PARAM_STR);
         $results->bindParam(":date", $date, PDO::PARAM_STR);
         $results->bindParam(":time", $time, PDO::PARAM_INT);
+        if($task_id){
+            $results->bindParam(":task_id", $task_id, PDO::PARAM_INT);
+        }
         $results->execute();
     }catch(Exception $e){
         echo "Error!: " . $e->getMessage() . "<br />";
         return false;
     }
     return true;
+}
+
+function get_task($task_id){
+    include 'connection.php';
+    
+    $sql = 'SELECT task_id, title, date, time, project_id FROM tasks
+            WHERE task_id = :id';
+    
+    try{
+        $results = $db->prepare($sql);
+        $results->bindParam(":id", $task_id, PDO::PARAM_INT);
+        $results->execute();
+    }catch(Exception $e){
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return $results->fetch();
 }
